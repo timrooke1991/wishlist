02d5fdf222f82e0043b726ec83b5e57b77a9f420
Tim Rooke <timrooke1991@gmail.com>
2021-06-08 08:03:19 +0100

    Actions to WishList and WishListItem added

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -11,8 +11,25 @@ export const WishListItem = types.model({
   price: types.number,
   image: types.optional(types.string, "")
 })
+  .actions((self) => ({
+    changeName(newName) {
+      self.name = newName;
+    },
+    changePrice(newPrice) {
+      self.price = newPrice;
+    },
+    changeImage(newImage) {
+      self.image = newImage;
+    }
+  }))
+
 
 export const WishList = types.model({
   items: types.optional(types.array(WishListItem), [])
 })
+  .actions((self) => ({
+    add(item) {
+      self.items.push(item);
+    }
+  }));
 
--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,26 +1,43 @@
-import { WishListItem, WishList  } from "./WishList";
+import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree";
+import { WishList, WishListItem } from "./WishList";
 
 it("can create a instance of a model", () => {
   const item = WishListItem.create({
-    name: "book",
-    price: 28.00,
-    image: "url"
+    name: "Chronicles of Narnia Box Set - C.S. Lewis",
+    price: 28.73
   });
 
-  expect(item.price).toBe(28.00);
-  expect(item.name).toBe("book");
-})
+  expect(item.price).toBe(28.73);
+  expect(item.image).toBe("");
+  item.changeName("Narnia");
+  expect(item.name).toBe("Narnia");
+});
 
-it('can create a wishlist', () => {
+it("can create a wishlist", () => {
   const list = WishList.create({
     items: [
       {
-        name: "book",
-        price: 28.00
+        name: "Chronicles of Narnia Box Set - C.S. Lewis",
+        price: 28.73
       }
     ]
-  })
+  });
 
   expect(list.items.length).toBe(1);
-  expect(list.items[0].price).toBe(28.00);
-})
+  expect(list.items[0].price).toBe(28.73);
+});
+
+it("can add new items", () => {
+  const list = WishList.create();
+  list.add(
+    WishListItem.create({
+      name: "Chesterton",
+      price: 10
+    })
+  );
+
+  expect(list.items.length).toBe(1);
+  expect(list.items[0].name).toBe("Chesterton");
+  list.items[0].changeName("Book of G.K. Chesterton");
+  expect(list.items[0].name).toBe("Book of G.K. Chesterton");
+});
